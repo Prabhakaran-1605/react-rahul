1f224464a2087b471a0a3e1368181e985f66aeda
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-12-02 12:46:57 +0530

    okie

--- react-11/src/App.js
+++ react-11/src/App.js
@@ -1,9 +1,18 @@
 import Message from './Components/Message'
+import Navbar from './Navbar/Navbar'
+import Product from './Product/Product'
+import Digital from './Digital/Digital'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 let App = () => {
     return <div>
-        <h1>App Component</h1>
-        <br />
-        <Message />
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/product" component={Product} />
+                <Route path="/digital" component={Digital} />
+            </Switch>
+
+        </Router>
     </div>
 }
 export default App
